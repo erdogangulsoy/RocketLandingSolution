93faff4f981985cb06292af0277cc59885efa9c7
Erdoğan Gülsoy <danko@codank.com>
2021-08-18 10:13:29 +0300

    Code cleanup

--- RocketLanding.App/Program.cs
+++ RocketLanding.App/Program.cs
@@ -6,8 +6,6 @@ namespace RocketLanding.App
     {
         static void Main(string[] args)
         {
-
-            RocketLanding.Domain.Platform p = new Domain.Platform(6, 66, 60, 5);
             RocketLanding.Domain.RocketLanding lp = new Domain.RocketLanding();
             lp.Land(new Domain.LandingPoint(5,5));
         }
